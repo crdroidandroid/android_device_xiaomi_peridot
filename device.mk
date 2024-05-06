0f7166fadb2c6df7182f7d8ae0cc72aeedc1fd07
thepriyanshujangid <priyanshujangid@yahoo.com>
2024-05-06 11:37:54 +0530

    Peridot: Include BCR if-exist
    * git clone https://github.com/thepriyanshujangid/vendor_bcr vendor/bcr
    
    Signed-off-by: Vikas Yaduvanshi <vikasyaduvanshi@gmail.com>

--- device.mk
+++ device.mk
@@ -13,7 +13,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Virtual A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
-# Dalvik vm configs
+# Basic Call Recorder
+$(call inherit-product-if-exists, vendor/bcr/bcr.mk)
+
+# Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # pKVM
