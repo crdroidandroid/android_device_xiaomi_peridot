0265844bb66628cb03a690e0d4654fbf5a0ac5fe
rio004 <riyon336@gmail.com>
2024-09-17 21:58:28 +0100

    peridot: Build common Xiaomi touch HIDL
    Change-Id: I8cfad2f0916b098b85c3a7584a8a2bac1be79b44

--- device.mk
+++ device.mk
@@ -517,6 +517,9 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor
 
 # Touchscreen
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
