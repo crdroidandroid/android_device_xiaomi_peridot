6920dca934300cf2e5dad071692a866f3f025408
Bruno Martins <bgcngm@gmail.com>
2024-10-01 14:42:47 +0100

    peridot: Resolve missing symbols on `libqcodec2_core`
    Change-Id: I300d78733f07b7fe1ba6b5066d02f49eefbeb54a

--- device.mk
+++ device.mk
@@ -482,6 +482,7 @@ PRODUCT_COPY_FILES += \
 
 # Shims
 PRODUCT_PACKAGES += \
+    libcodec2_shim \
     libhidlbase_shim
 
 # Telephony
--- extract-files.sh
+++ extract-files.sh
@@ -82,6 +82,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i "s/\/odm\/bin\//\/vendor\/bin\//g" "${2}"
             ;;
+        vendor/lib64/libqcodec2_core.so)
+	    [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "libcodec2_shim.so" "${2}"
+            ;;
         vendor/lib64/vendor.libdpmframework.so)
 	    [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --add-needed "libhidlbase_shim.so" "${2}"
