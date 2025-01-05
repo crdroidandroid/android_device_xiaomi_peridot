04fa96ebf4de70eafae582e5d56b64e085c23fd7
raghavt20 <raghavt20@aospa.co>
2025-01-05 09:39:44 +0530

    peridot: Build required vendor variants for mi fingerprint daemon(mfpd)
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- device.mk
+++ device.mk
@@ -198,6 +198,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
+    android.hardware.biometrics.common.thread.vendor \
+    android.hardware.biometrics.common.util.vendor \
     android.hardware.biometrics.common.thread \
     android.hardware.biometrics.common.util \
     android.hardware.biometrics.fingerprint-V3-ndk.vendor
