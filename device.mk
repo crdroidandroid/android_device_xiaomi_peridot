f47ca6d4ecb42fc5ee7b99022121770fa11ea5f8
Kmiit <kmiit@foxmail.com>
2025-02-12 09:25:27 +0000

    peridot: fingerprint: Clean up useless fingerprint code
    Adapt for peridot
    Signed-off-by: GuidixX <guidixm@gmail.com>
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- device.mk
+++ device.mk
@@ -207,7 +207,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint-service.xiaomi
+    android.hardware.biometrics.fingerprint-service.peridot
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
