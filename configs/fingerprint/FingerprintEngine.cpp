ba7ce3f2d7d24067b5601808921ee9eb7c5418b5
Guidix <guidixm@gmail.com>
2025-02-20 18:51:11 +0200

    peridot: fingerprint: Setup correct peridot vendor code
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- configs/fingerprint/FingerprintEngine.cpp
+++ configs/fingerprint/FingerprintEngine.cpp
@@ -122,10 +122,10 @@ void FingerprintEngine::onAcquired(int32_t result, int32_t vendorCode) {
     if (result != FINGERPRINT_ACQUIRED_VENDOR) {
         setFingerStatus(false);
         if (result == FINGERPRINT_ACQUIRED_GOOD) setFodStatus(FOD_STATUS_OFF);
-    } else if (vendorCode == 20 || vendorCode == 22) {
+    } else if (vendorCode == 21 || vendorCode == 23) {
         /*
-         * vendorCode = 20 waiting for fingerprint authentication
-         * vendorCode = 22 waiting for fingerprint enroll
+         * vendorCode = 21 waiting for fingerprint authentication
+         * vendorCode = 23 waiting for fingerprint enroll
          */
         setFodStatus(FOD_STATUS_ON);
     } else if (vendorCode == 44) {
