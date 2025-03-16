8c9990f9701f1e3885358d589fa2310b33ae535b
raghavt20 <raghavt20@aospa.co>
2025-03-16 15:30:03 +0530

    peridot: Migrate mount point symlink creation to blueprint
    Change-Id: I565d87cd9e63454e54834aab43247f84710786e7
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Signed-off-by: Vikas-V2 <vikasyadav5073@gmail.com>

--- device.mk
+++ device.mk
@@ -401,6 +401,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
 
+# Mountpoints
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint \
+    vendor_modem_firmware_mountpoint \
+    vendor_vm-system_mountpoint
+
 # Network
 PRODUCT_PACKAGES += \
     android.system.net.netd-V1-ndk.vendor \
