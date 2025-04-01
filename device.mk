16779f5445205a16aba41263b5eba87c91b35931
kenway214 <kenway214@outlook.com>
2025-04-01 17:31:07 +0530

    peridot: Drop android.hardware.audio.sounddose-vendor-impl
    Signed-off-by: kenway214 <kenway214@outlook.com>

--- device.mk
+++ device.mk
@@ -56,7 +56,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio@7.1-impl \
-    android.hardware.audio.sounddose-vendor-impl \
     android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
